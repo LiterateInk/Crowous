a340e7025b655fd98d85f6f832bb1d1e47371903
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-14 18:00:23 +0200

    chore: export main methods, types and classes

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Crous } from "~/models/Crous";
+export { Restaurant, PaymentMethod, RestaurantType } from "~/models/Restaurant";
+export { Meal, type MealCategory } from "~/models/Meal";
+export { getAvailableCrous } from "~/utils/getAvailableCrous";
+export { getRestaurants } from "~/utils/getRestaurants";
