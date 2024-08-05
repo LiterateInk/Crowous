204547fbf83d2ce10c8695fcbce4fb7529120766
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-05 04:12:28 +0200

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export { Crous } from "~/models/Crous";
-export { Restaurant, PaymentMethod, RestaurantType } from "~/models/Restaurant";
-export { Meal, type MealCategory, MealTiming } from "~/models/Meal";
-export { getAvailableCrous } from "~/utils/getAvailableCrous";
-export { getRestaurants } from "~/utils/getRestaurants";
+export * from "~/models";
+export * from "~/services";
