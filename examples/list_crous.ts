a18fa136af712ed6f3f9861c54e23a34c0ceac67
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-05 04:12:16 +0200

    docs(examples): match rust examples

--- examples/list_crous.ts
+++ examples/list_crous.ts
@@ -1,13 +1,12 @@
 import { getAvailableCrous } from "../src";
+//                                ^^^^^^^^
+// Replace with:             from "crowous";
+// when using the package.
 
-/**
- * Simply list every available Crous.
- * Returned object can be used to find restaurants.
- */
 void async function main() {
   const availableCrous = await getAvailableCrous();
 
   for (const crous of availableCrous) {
-    console.log(`+ ${crous.name} with the identifier "${crous.identifier}"`);
+    console.log(`${crous.name} (${crous.identifier})`);
   }
 }();
