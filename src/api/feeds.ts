02ecffc600b392738759a619b711765618d808b4
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-08-11 22:52:34 +0200

    fix(api): make `feeds` arrow function

--- src/api/feeds.ts
+++ src/api/feeds.ts
@@ -3,10 +3,10 @@ import { Request } from "~/core/request";
 import { decodeFeed } from "~/decoders/feed";
 import type { Feed } from "~/models";
 
-export async function feeds (fetcher: Fetcher = defaultFetcher): Promise<Array<Feed>> {
+export const feeds = async (fetcher: Fetcher = defaultFetcher): Promise<Array<Feed>> => {
   const request = new Request("feeds.json");
   const { content } = await fetcher(request);
   const { results } = JSON.parse(content);
 
   return results.map(decodeFeed);
-}
+};
