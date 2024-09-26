bd755817f52a4830085ecf099dd1d74e5e4fcf38
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-26 22:13:27 +0200

    chore: add publish

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,12 +1,15 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.vanniktech.maven.publish.SonatypeHost
+
+val libraryName = "Crowous"
+version = "0.1.0"
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
-
-version = "0.1.0"
 
 kotlin {
     jvm()
@@ -46,10 +49,48 @@ kotlin {
     }
 }
 
+val groupName = "ink.literate." + libraryName.lowercase()
+group = groupName
+
 android {
-    namespace = "ink.literate.crowous"
+    namespace = groupName
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
 }
+
+mavenPublishing {
+    coordinates(groupName, libraryName.lowercase(), version.toString())
+
+    pom {
+        name.set(libraryName)
+        description.set("A wrapper for the Crous Mobile internal API.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/LiterateInk/$libraryName")
+        licenses {
+            license {
+                name.set("GPL-3.0-or-later")
+                url.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+                distribution.set("https://www.gnu.org/licenses/gpl-3.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("literateink")
+                name.set("LiterateInk")
+                url.set("https://literate.ink")
+            }
+        }
+        scm {
+            url.set("https://github.com/LiterateInk/$libraryName")
+            connection.set("scm:git:git://github.com/LiterateInk/$libraryName.git")
+            developerConnection.set("scm:git:ssh://git@github.com/LiterateInk/$libraryName.git")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
+    signAllPublications()
+}
+
+
